594a6b792d1eea92f5d9064cd5882369a93d0475
Amit <heroku467@gmail.com>
2025-03-27 00:35:47 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-API_ID = 123456  # api id
-API_HASH = "ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # api hash
+API_ID = 22012880  # api id
+API_HASH = "5b0e07f5a96d48b704eb9850d274fe1d"  # api hash
 
-BOT_TOKEN = "123456:ABC-DEF1234ghIkl-zyx57W2v1u123ew11"  # bot token
+BOT_TOKEN = "8113183687:AAGY2SeZhdaBqDIjyGkQL2e6GiH5w6JWBro"  # bot token
 
 
 # REDIS
@@ -9,15 +9,15 @@ HOST = "localhost"  # redis host uri
 PORT = 6379  # redis port
 PASSWORD = ""  # redis password
 
-PRIVATE_CHAT_ID = -1001234567890  # CHAT WHERE YOU WANT TO STORE VIDEOS
+PRIVATE_CHAT_ID = -1002544207724  # CHAT WHERE YOU WANT TO STORE VIDEOS
 # COOKIE FOR AUTHENTICATION (get from chrome dev tools) ex: "PANWEB=1; csrfToken=;
 COOKIE = ""
-ADMINS = [1317173146]
+ADMINS = [7150972327]
 
 
 BOT_USERNAME = "teraboxdown_bot"
 
 # Force user to join this channel. (make sure you have promoted the bot on this chat.)
-FORCE_LINK = "@RoldexVerse"
+FORCE_LINK = "@Stellar_bots"
 
 PUBLIC_EARN_API = ""  # https://publicearn.com/api
